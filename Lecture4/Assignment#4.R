8e0b81645fd18c3e6699d5ff1f0dd6f87fa81c80
Sayed A Rizvi <s_amir_72@yahoo.com>
2017-02-11 10:02:47 -0800

    Assignment # 4 Part 2

--- Lecture4/Assignment#4.R
+++ Lecture4/Assignment#4.R
@@ -64,15 +64,18 @@ ks.test(price,logprice)
 #Stratifying the data.
 n = 5
 #Group By Fuel Type
-stratified_pricefuel = auto.price %>% group_by(fuel.type) %>% sample_n(n, replace = FALSE)
+#stratified_pricefuel = auto.price %>% group_by(fuel.type) %>% sample_n(n, replace = FALSE)
+stratified_pricefuel = auto.price %>% group_by(fuel.type)
 logpricegroupedbyfueltypegas = stratified_pricefuel %>% filter(fuel.type == 'gas')
 logpricegroupedbyfueltypediesel = stratified_pricefuel %>% filter(fuel.type == 'diesel')
 #Group by Aspiration
-stratified_priceasp = auto.price %>% group_by(aspiration) %>% sample_n(n, replace = FALSE)
+#stratified_priceasp = auto.price %>% group_by(aspiration) %>% sample_n(n, replace = FALSE)
+stratified_priceasp = auto.price %>% group_by(aspiration)
 logpricegroupedbyaspirationstd = stratified_priceasp %>% filter(aspiration == 'std')
 logpricegroupedbyaspirationsturbo = stratified_priceasp %>% filter(aspiration == 'turbo')
 #Group by drive wheel
-stratified_pricedrive = auto.price %>% group_by(drive.wheels) %>% sample_n(n, replace = FALSE)
+#stratified_pricedrive = auto.price %>% group_by(drive.wheels) %>% sample_n(n, replace = FALSE)
+stratified_pricedrive = auto.price %>% group_by(drive.wheels)
 logpricegroupedbydrivewheelfwd = stratified_pricedrive %>% filter(drive.wheels == 'fwd')
 logpricegroupedbydrivewheelrwd = stratified_pricedrive %>% filter(drive.wheels == 'rwd')
 logpricegroupedbydrivewheel4wd = stratified_pricedrive %>% filter(drive.wheels == '4wd')
@@ -90,16 +93,63 @@ t.test(logpricegroupedbydrivewheelrwd$normalized_logprice, auto.price$normalized
 t.test(logpricegroupedbyaspirationstd$normalized_logprice, auto.price$normalized_logprice, alternative = "two.sided")
 t.test(logpricegroupedbyaspirationsturbo$normalized_logprice, auto.price$normalized_logprice, alternative = "two.sided")
 
+#K-S Test
+#Gauge impact of fuel type on price
+ks_df <- data.frame()
+ks_logpricegroupedbyfueltypegas <- ks.test(logpricegroupedbyfueltypegas$normalized_logprice, auto.price$normalized_logprice)
+ks_logpricegroupedbyfueltypegas
+ks_df[1,1] <- ks_logpricegroupedbyfueltypegas$p.value
+ks_df[1,2] <- ks_logpricegroupedbyfueltypegas$statistic
+ks_logpricegroupedbyfueltypediesel <- ks.test(logpricegroupedbyfueltypediesel$normalized_logprice, auto.price$normalized_logprice)
+ks_logpricegroupedbyfueltypediesel
+ks_df[2,1] <- ks_logpricegroupedbyfueltypediesel$p.value
+ks_df[2,2] <- ks_logpricegroupedbyfueltypediesel$statistic
+#Gauge impact of drive wheel on price
+ks_logpricegroupedbydrivewheel4wd <- ks.test(logpricegroupedbydrivewheel4wd$normalized_logprice, auto.price$normalized_logprice)
+ks_logpricegroupedbydrivewheel4wd
+ks_df[3,1] <- ks_logpricegroupedbydrivewheel4wd$p.value
+ks_df[3,2] <- ks_logpricegroupedbydrivewheel4wd$statistic
+ks_logpricegroupedbydrivewheelfwd <- ks.test(logpricegroupedbydrivewheelfwd$normalized_logprice, auto.price$normalized_logprice)
+ks_logpricegroupedbydrivewheelfwd
+ks_df[4,1] <- ks_logpricegroupedbydrivewheelfwd$p.value
+ks_df[4,2] <- ks_logpricegroupedbydrivewheelfwd$statistic
+ks_logpricegroupedbydrivewheelrwd <- ks.test(logpricegroupedbydrivewheelrwd$normalized_logprice, auto.price$normalized_logprice)
+ks_logpricegroupedbydrivewheelrwd
+ks_df[5,1] <- ks_logpricegroupedbydrivewheelrwd$p.value
+ks_df[5,2] <- ks_logpricegroupedbydrivewheelrwd$statistic
+#Gauge impact of aspiration on price
+ks_logpricegroupedbyaspirationstd <- ks.test(logpricegroupedbyaspirationstd$normalized_logprice, auto.price$normalized_logprice)
+ks_logpricegroupedbyaspirationstd
+ks_df[6,1] <- ks_logpricegroupedbyaspirationstd$p.value
+ks_df[6,2] <- ks_logpricegroupedbyaspirationstd$statistic
+ks_logpricegroupedbyaspirationsturbo <- ks.test(logpricegroupedbyaspirationsturbo$normalized_logprice, auto.price$normalized_logprice)
+ks_logpricegroupedbyaspirationsturbo
+ks_df[7,1] <- ks_logpricegroupedbyaspirationsturbo$p.value
+ks_df[7,2] <- ks_logpricegroupedbyaspirationsturbo$statistic
+headers <-c("pvalue","statistic")
+names(ks_df) <- headers
+
+
+
+require(ggplot2)
+#Line Plot
+ggplot(ks_df, aes(ks_df$pvalue, ks_df$statistic)) + geom_line() + ggtitle('Line plot of P-value vs KS Statistics') +
+xlab('P-Value') + ylab('KS Statistics')
+
 #Anova Test
 #Stratify data as per number of doors and body style.
 
 #Group By number of doors
-stratified_pricedoors = auto.price %>% group_by(num.of.doors) %>% filter(num.of.doors != '?') %>% sample_n(n, replace = FALSE)
+stratified_pricedoors = auto.price %>% group_by(num.of.doors) %>% filter(num.of.doors != '?')
 #Group by body style
 stratified_pricebody = auto.price %>% group_by(body.style) %>% sample_n(n, replace = FALSE)
 
-df_aov_door = aov(auto.price$normalized_logprice ~ auto.price$num.of.doors, data = stratified_pricedoors)
+#stratified_pricedoors$num.of.doors = factor(stratified_pricedoors$num.of.doors) # Make sure your groups are a factor (for further analysis below)
+boxplot(stratified_pricedoors$normalized_logprice ~ stratified_pricedoors$num.of.doors)
+
+df_aov_door = aov(stratified_pricedoors$normalized_logprice ~ stratified_pricedoors$num.of.doors, data = stratified_pricedoors)
 summary(df_aov_door)
 
-df_aov_door = aov(auto.price$normalized_logprice ~ auto.price$drive.wheels, data = stratified_pricedoors)
-summary(df_aov_door)
+tukey_anova_door = TukeyHSD(df_aov_door)  # Tukey's Range test:
+tukey_anova_door
+
